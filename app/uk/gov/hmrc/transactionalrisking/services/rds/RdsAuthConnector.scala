5b6b0961d2fa0edf1014cee5dc4dc2c666db0178
basotia <16934889+basotia@users.noreply.github.com>
2022-11-23 14:58:35 +0000

    removed json conversion

--- app/uk/gov/hmrc/transactionalrisking/services/rds/RdsAuthConnector.scala
+++ app/uk/gov/hmrc/transactionalrisking/services/rds/RdsAuthConnector.scala
@@ -50,7 +50,7 @@ class DefaultRdsAuthConnector @Inject() (@Named("nohook-auth-http-client") http:
     //val body = s"&grant_type=${URLEncoder.encode(appConfig.rdsAuthCredential.grant_type, "UTF-8")}"
     val body = /*s"client_id=${URLEncoder.encode(appConfig.rdsAuthCredential.client_id, "UTF-8")}" +
       s"&client_secret=${URLEncoder.encode(appConfig.rdsAuthCredential.client_secret, "UTF-8")}" +*/
-      s"&grant_type=${URLEncoder.encode(appConfig.rdsAuthCredential.grant_type, "UTF-8")}"+
+      s"grant_type=${URLEncoder.encode(appConfig.rdsAuthCredential.grant_type, "UTF-8")}"+
       s"&code=${URLEncoder.encode("o1a2fXTJVU", "UTF-8")}"
 
 val credentials = s"${URLEncoder.encode(appConfig.rdsAuthCredential.client_id, "UTF-8")}"+
@@ -63,7 +63,7 @@ val credentials = s"${URLEncoder.encode(appConfig.rdsAuthCredential.client_id, "
           "Accept"-> "application/json",
           "Authorization"-> s"Basic $credentials"))
         .map { response =>
-          logger.info(s"RDSConnector :: response is ${response.json}")
+          logger.info(s"RDSConnector :: response is $response")
           response.status match {
             case ACCEPTED        =>
               logger.info(s"RDSConnector :: ACCEPTED reponse")
