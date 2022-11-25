5e7feb911a450cc097621b9c9a1ecd8b58a1e018
basotia <16934889+basotia@users.noreply.github.com>
2022-11-25 14:04:58 +0000

    logs to debug NRS connectivity

--- app/uk/gov/hmrc/transactionalrisking/services/nrs/NrsConnector.scala
+++ app/uk/gov/hmrc/transactionalrisking/services/nrs/NrsConnector.scala
@@ -51,7 +51,7 @@ class NrsConnector @Inject()(val httpClient: HttpClient,
     }
 
     retry(appConfig.nrsRetries, retryCondition) { attemptNumber =>
-      logger.info(s"$correlationId::[submit] Attempt $attemptNumber NRS submission: sending POST request to $url")
+      logger.info(s"$correlationId::[submit] Attempt $attemptNumber NRS submission: sending POST request to $url with key $apiKey)")
 
       httpClient
         .POST[NrsSubmission, HttpResponse](s"$url", nrsSubmission, Seq("X-API-Key" -> apiKey))
