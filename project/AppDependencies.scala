38a5cb33d7338ee7bac4adeffa870ab4ddf03004
darran eteo <107866129+deteohm@users.noreply.github.com>
2022-12-19 10:05:44 +0000

    Trdt 706 2 (#56)
    * removed moved files
    
    * No url encoding
    
    * Use apache codecs in both service and stubs
    
    * Removed println from VersionRouting map
    log featrures
    
    * changed path from /indivduals/self-assist/report to /submission
    
    * TRDT-706 Removing duplicate build.properties file
    
    * TRDT-706 update sbt version and clean up imports
    
    * TRDT-706 Update dependencies and modules
    
    * TRDT-706 Update dependencies and sbt plugins
    
    * TRDT-706 Update Play Framework version
    
    Co-authored-by: Adam Conder <adam.conder@digital.hmrc.gov.uk>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,22 +1,22 @@
-import play.core.PlayVersion
-import play.sbt.PlayImport._
-import sbt.Keys.libraryDependencies
 import sbt._
 
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.24.0",
-    "org.typelevel"                %% "cats-core"                 % "2.7.0",
+    "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.12.0",
+    "org.typelevel"           %% "cats-core"                  % "2.7.0",
+    "commons-codec"           % "commons-codec"               % "1.15"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "5.24.0"             % "test, it",
-
-    "de.leanovate.play-mockws"      %% "play-mockws"                % "2.8.1"              % "test, it",
-    "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"             % "test, it",
-    "org.scalamock"          %% "scalamock"                   % "5.2.0"              % "test, it",
-    "com.miguno.akka"        %% "akka-mock-scheduler"         % "0.5.5"              % "test, it"
-
+    "uk.gov.hmrc"              %% "bootstrap-test-play-28"     % "7.12.0"             % "test, it",
+    "de.leanovate.play-mockws" %% "play-mockws"                % "2.8.1"              % "test, it",
+    "org.scalamock"            %% "scalamock"                  % "5.2.0"              % "test, it",
+    "com.miguno.akka"          %% "akka-mock-scheduler"        % "0.5.5"              % "test, it",
+    "org.scalatestplus"        %% "mockito-4-5"                % "3.2.12.0"           % "test, it",
+  // NOTE: See https://github.com/vsch/flexmark-java to understand that
+  //       versions 0.62.2 or below, Java 8 or above, Java 9+ compatible.
+  //       while for Versions 0.64.0 or above, Java 11 or above.
+  "com.vladsch.flexmark"        % "flexmark-profile-pegdown"    % "0.62.2"             % "test, it"
   )
 }
