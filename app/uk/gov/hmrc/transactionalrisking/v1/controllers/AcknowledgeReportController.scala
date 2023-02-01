5051f785be33ceaf557619eb6c7518f9ae3ab395
basotia <16934889+basotia@users.noreply.github.com>
2023-02-01 13:53:18 +0000

    Error handling fix for forbidden response from RDS

--- app/uk/gov/hmrc/transactionalrisking/v1/controllers/AcknowledgeReportController.scala
+++ app/uk/gov/hmrc/transactionalrisking/v1/controllers/AcknowledgeReportController.scala
@@ -94,7 +94,8 @@ class AcknowledgeReportController @Inject()(
   }
 
   def errorHandler(errorWrapper: ErrorWrapper, correlationId: String): Future[Result] = (errorWrapper.error,errorWrapper.errors) match {
-    case (ServerError | DownstreamError |ServiceUnavailableError|ForbiddenDownstreamError,_) => Future.successful(InternalServerError(convertErrorAsJson(DownstreamError)))
+    case (ServerError | DownstreamError |ServiceUnavailableError,_) => Future.successful(InternalServerError(convertErrorAsJson(DownstreamError)))
+    case (ForbiddenDownstreamError,_) => Future.successful(Forbidden(convertErrorAsJson(ForbiddenDownstreamError)))
     case (FormatReportIdError,_) => Future.successful(BadRequest(convertErrorAsJson(FormatReportIdError)))
     case (ClientOrAgentNotAuthorisedError,_) => Future.successful(Forbidden(convertErrorAsJson(ClientOrAgentNotAuthorisedError)))
     case (NinoFormatError,_) => Future.successful(BadRequest(convertErrorAsJson(NinoFormatError)))
