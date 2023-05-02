191f3f34f875cf09461b429642a2291c590866c3
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2023-05-02 14:53:31 +0100

    MTDSA-16319: Tests for request models (#138)
    * Initial push for added/Updated/Fixed tests
    
    * Added/Updated tests for NRS and Errors logic
    
    * Tests for request models
    
    * Updated logs

--- app/uk/gov/hmrc/selfassessmentassist/utils/DateUtils.scala
+++ app/uk/gov/hmrc/selfassessmentassist/utils/DateUtils.scala
@@ -19,13 +19,13 @@ package uk.gov.hmrc.selfassessmentassist.utils
 import play.api.libs.json._
 
 import java.time.format.DateTimeFormatter
-import java.time.{LocalDate, OffsetDateTime}
+import java.time.{LocalDate, LocalDateTime, OffsetDateTime, ZoneOffset}
 import scala.util.{Failure, Success, Try}
 
 object DateUtils {
 
 
-  val isoInstantDatePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
+  val isoInstantDatePattern: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss'Z'")
   val isoInstantDateRegex = """(\d){4}-(\d){2}-(\d){2}T(\d){2}:(\d){2}:(\d){2}Z"""
   val dateTimePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
   val datePattern = DateTimeFormatter.ofPattern("yyyy-MM-dd")
@@ -57,6 +57,38 @@ object DateUtils {
   implicit def isoInstantDateReads: Reads[OffsetDateTime] = new Reads[OffsetDateTime] {
     override def reads(json: JsValue): JsResult[OffsetDateTime] =
       Try(JsSuccess(OffsetDateTime.parse(json.as[String], isoInstantDatePattern), JsPath)).getOrElse(JsError())
+  }
+
+  implicit val offsetDateTimeFromLocalDateTimeFormatReads: Reads[OffsetDateTime] = { json =>
+    json.as[String].parseOffsetDateTimeFromLocalDateTimeFormat() match {
+      case Right(value) => JsSuccess(value)
+      case Left(error) => JsError("not a valid date " + error.errorMessage)
+    }
+  }
+
+  implicit class LocalDateTimeExtensions(localDateTime: LocalDateTime) {
+    def utcOffset: OffsetDateTime = localDateTime.atOffset(ZoneOffset.UTC)
+  }
+
+  implicit class StringExtensions(string: String) {
+
+    def parseOffsetDateTimeFromLocalDateTimeFormat(formatter: DateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME)
+    : Either[DateError, OffsetDateTime] = {
+
+      Try(LocalDateTime.parse(string, formatter).utcOffset) match {
+        case Success(offsetDateTime) => Right(offsetDateTime)
+        case Failure(exception) => Left(DateParseError(exception.getMessage, string))
+      }
+    }
+  }
+
+  sealed trait DateError {
+    val errorMessage: String
+    val dateFailedToParse: String
+  }
+  case class DateParseError(e: String, dateFailed: String) extends DateError {
+    val dateFailedToParse: String = dateFailed
+    val errorMessage: String = e
   }
 
 
